bbec7f531c53497a7f91096886c8dd86216ab23e
Rania Amina <reaamina@gmail.com>
2017-03-30 23:32:08 +0700

    add files

---
+++ solid-splash-screen/inkscape-splash-wrapper.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import subprocess
+import time
+
+application = "inkscape"
+path = "./inkscape-splash.py"
+
+subprocess.Popen([application])
+subprocess.Popen(["python3", path])
+
+while True:
+    time.sleep(0.5)
+    try:
+        pid = subprocess.check_output(["pidof", application]).decode("utf-8").strip()
+        w_list = subprocess.check_output(["wmctrl", "-lp"]).decode("utf-8")
+        if pid in w_list:
+            splashpid = [l.split()[2] for l in w_list.splitlines()\
+                         if "inkscape-splash" in l][0]
+            subprocess.Popen(["kill", splashpid])
+            break
+    except subprocess.CalledProcessError:
+        pass	
+
---
+++ solid-splash-screen/inkscape-splash.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, Gdk, Pango
+
+class Splash(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="inkscape-splash")
+        maingrid = Gtk.Grid()
+        self.add(maingrid)
+
+        image = Gtk.Image()
+        # set the path to the image below
+        image.set_from_file("./inkscape-splash.png")
+        maingrid.attach(image, 1, 0, 1, 1)
+	
+
+def splashwindow():
+    window = Splash()
+    window.set_decorated(False)
+    window.set_resizable(False)
+    window.override_background_color(Gtk.StateType.NORMAL, Gdk.RGBA(1,1,1,5))
+    window.modify_fg(Gtk.StateFlags.NORMAL, Gdk.color_parse("grey"))
+    window.set_opacity(5.0)
+    window.set_position(Gtk.WindowPosition.CENTER)
+    window.show_all()
+    Gtk.main()
+
+splashwindow()
